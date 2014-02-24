943a6aeefb403aeccefaea0f5fcac6c580464b4d
Ryan Czaja <ryanczaja@mac.com>
2014-02-24 23:26:46 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# HopHeads
+
+<p>A social networking platform for craft beer drinkers and enthusiasts.</p>
+
+Made with my mentor at [Bloc](http://bloc.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hopheads::Application.config.secret_token = '581eafe60dd953849a77ec2ef5b010f5e26c963ac27c1de06ea1934e2e900079811cbbd534a3eb5641e80aa3e0f48c859c21b719d3fbcb55195bb0c45e812bb1'
